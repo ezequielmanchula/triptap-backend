fdf2861861f97e4e8de36e168e4f30f09efa8d55
Ezel <ezequielmanchula3697@gmail.com>
2025-11-01 19:50:46 +0000

    feat(profile): add editable form and filter empty fields before PATCH

--- api/src/user/dto/update-user.dto.ts
+++ api/src/user/dto/update-user.dto.ts
@@ -1,28 +1,23 @@
-import { IsEmail, IsOptional, IsString, MinLength, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class UpdateUserDto {
   @IsOptional()
   @IsString()
-  @IsNotEmpty()
   name?: string;
 
   @IsOptional()
   @IsString()
-  @IsNotEmpty()
   lastName?: string;
 
   @IsOptional()
   @IsEmail()
-  @IsNotEmpty()
   email?: string;
 
   @IsOptional()
   @IsString()
-  @IsNotEmpty()
   phone?: string;
 
   @IsOptional()
   @MinLength(8)
-  @IsNotEmpty()
   password?: string;
 }
